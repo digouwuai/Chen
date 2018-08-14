2ade709a6b518c80346d08a1eaf6d8aa2b388696
digouwuai <42329317+digouwuai@users.noreply.github.com>
2018-08-14 08:50:45 +0800

    Update zuwengjian.py

--- zuwengjian.py
+++ zuwengjian.py
@@ -1,1 +1,14 @@
-....
+# -*- coding: UTF-8 -*-
+from flask import Flask,request,make_response,redirect,render_template
+import MySQLdb
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''return redirect('https://www.csdn.net/')#（redirect）直接进入这个网站'''
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
